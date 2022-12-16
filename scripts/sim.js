f3a6b820303b115a2e600bcbe9d5f5dde75eafd4
fnorbert87 <f.norbert87@gmail.com>
2022-12-16 09:03:24 +0100

    12.16.09.03.

--- scripts/sim.js
+++ scripts/sim.js
@@ -1,4 +1,22 @@
-for (i in p16f628)
-{
-    console.log(p16f628[i])
-}
+let PICset = [];
+let tempList = [];
+
+tempList = PICini.replace(/\n/g, '\n').substring(1).split(/\n/);
+
+tempList.forEach((element) => {
+  if (element.substring(0, 1) != '#' && element.length > 0) {
+    if (element.includes('=')) {
+      temp = element.split('=');
+      if (temp[0] != 'SFR' && temp[0] != 'SFRFLD') {
+        PICset[temp[0]] = temp[1].includes(',') ? temp[1].split(',') : temp[1];
+      } else {
+        if (temp[0] in PICset == false) 
+            PICset[temp[0]] = [];
+        PICset[temp[0]].push(temp[1].split(','));
+      }
+    }
+  }
+});
+
+//console.log(PICset[0].length)
+console.log(PICset);
